f0dd87506e0ced04c0526d34bfebc48b5ba20d5a
chaim <chaim@bienenfeld.org>
2023-10-30 16:41:13 -0400

    fixed spelling of radius.

--- radius.java
+++ radius.java
@@ -1,13 +1,13 @@
 
 // import java.util.*;
 import java.util.Scanner;
-public class radios {
+public class radius {
     public static void ex1() {
         Scanner y= new Scanner(System.in);
-        System.out.println( "what is the radios");    
-        double radios = y.nextDouble();
-        double shetach =  (3.14 * radios * radios);
-        System.out.println("your hekef is "+2*3.14*radios);
+        System.out.println( "what is the radius");    
+        double radius = y.nextDouble();
+        double shetach =  (3.14 * radius * radius);
+        System.out.println("your hekef is "+2*3.14*radius);
         System.out.print("your shetach is "+shetach);
         y.close(); 
 
@@ -23,9 +23,9 @@ public static void ex2() {
     final int Mushroomprice = 2;
     final int Cheeseprice = 3;
     
-    System.out.print("Welcome to the Bread Shop");
+    System.out.print("Welcome to the Bread Shop.\n");
     System.out.println(" The bread costs $12:");
-    System.out.println(" If you want toppings we have:");
+    System.out.println(" If you want toppings, we have:");
     System.out.println(" Olives $"+ Oliveprice +"\n Corn $" + Cornprice + " \n Mushrooms $"+ Mushroomprice +"\n and\n Cheese $"+Cheeseprice+"\n and if you dont want any (or any more) write 'None'");
     
     // List<String> toppings = new ArrayList<String>();
